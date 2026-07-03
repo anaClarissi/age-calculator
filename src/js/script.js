08173e298b4b8feb3b03d6315a0fb8444b3b6e99
Ana Clarissi <anaclarissicruz@gmail.com>
2026-07-03 12:51:31 -0300

    feat: error states and years calculator logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,262 @@
-console.log("Hello World!");
+/*==========================*/
+/*       ERRORS OBJECT      */
+/*==========================*/
+
+const errors = {
+    empty: "This field is required",
+    invalid: {
+        day: "Must be a valid day",
+        month: "Must be a valid month",
+        year: "Must be a valid year"
+    },
+    form: "Must be a valid date",
+};
+
+
+
+/*==========================*/
+/*    FORM EVENT SUBMIT     */
+/*==========================*/
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+
+    const year = document.querySelector("#year");
+    
+    const day = document.querySelector("#day");
+    
+    const month = document.querySelector("#month");
+
+
+    const isYearValid = checkYear(year);
+    
+    const isMonthValid = checkMonth(month);
+    
+    const isDayValid = checkDay(day);
+
+
+    hiddenError(day);
+    
+    if (isYearValid, isMonthValid, isDayValid) {
+
+        const isFullDateValid = checkFullDate(year, month, day);
+
+        if (isFullDateValid) showResult(year, month, day);
+
+    }
+
+
+});
+
+
+
+
+/*==========================*/
+/*     INPUT EVENT CLICK    */
+/*==========================*/
+
+const inputs = document.querySelectorAll('input[type="number"]');
+
+inputs.forEach(input => {
+
+    input.addEventListener("click", () => hiddenError(input));
+
+});
+
+
+
+
+/*==========================*/
+/*         FUNCTIONS        */
+/*==========================*/
+
+function showResult(year, month, day) {
+
+    const currentDate = new Date();
+
+    const birthdayDate = new Date(year.value, month.value, day.value);
+
+
+    let yearsDate = currentDate.getFullYear() - birthdayDate.getFullYear();
+
+    let monthsDate = currentDate.getMonth() - birthdayDate.getMonth();
+
+    let daysDate = currentDate.getDate() - birthdayDate.getDate();
+
+    if (monthsDate < 0 || (monthsDate === 0 && daysDate < 0)) {
+
+        yearsDate--;
+
+        monthsDate = monthsDate < 0 ? monthsDate + 12 : monthsDate;
+
+    }
+
+    if (daysDate < 0) {
+
+        const antecessorMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+
+        daysDate = antecessorMonth.getDate() + daysDate;
+
+    }
+
+
+    const resultYear = document.querySelector(".result-years");
+
+    const resultMonth = document.querySelector(".result-months");
+
+    const resultDay = document.querySelector(".result-days");
+
+
+    resultYear.textContent = yearsDate;
+
+    resultMonth.textContent = monthsDate;
+
+    resultDay.textContent = daysDate;
+
+}   
+
+function checkFullDate(year, month, day) {
+
+    year = Number(year.value);
+
+    month = Number(month.value);
+
+    day = Number(day.value);
+
+    const date = new Date(year, month - 1, day);
+
+    if (
+        date.getFullYear() === year &&
+        date.getMonth() === (month - 1) &&
+        date.getDate() === day
+    ) {
+
+        return true;
+
+    }
+
+    showErrorForm();
+
+    return false;
+
+}
+
+function checkDay(day) {
+
+    if (isEmpty(day)) {
+
+        showError(day, errors.empty);
+
+        return false;
+
+    }
+
+    if (day.value > 31) {
+
+        isValid = false;
+
+        showError(day, errors.invalid.day);
+
+        return false;
+
+    }
+
+    return true;
+
+}
+
+function checkMonth(month) {
+
+    if(isEmpty(month)) {
+
+        showError(month, errors.empty);
+
+        return false;
+
+    }
+
+    if (month.value > 12) {
+
+        showError(month, errors.invalid.month);
+
+        return false;
+
+    }
+
+    return true;
+
+}
+
+function checkYear(year) {
+
+    const currentYear = new Date().getFullYear();
+
+    if (isEmpty(year)) {
+
+        showError(year, errors.empty);
+
+        return false;
+
+    }
+
+    if (year.value > currentYear) {
+
+        showError(year, errors.invalid.year);
+
+        return false;
+
+    }
+
+    return true;
+
+}
+
+function isEmpty(input) {
+
+    return input.value === "";
+
+}
+
+function showError(input, errorText) {
+
+    hiddenError(input);
+
+    const errorMessage = document.querySelector(`#${input.id}-error-message`);
+
+    errorMessage.textContent = errorText;
+
+    errorMessage.hidden = false;
+
+    input.classList.add("error");
+
+}
+
+function showErrorForm() {
+
+    const errorMessage = document.querySelector("#day-error-message");
+
+    errorMessage.textContent = errors.form;
+
+    errorMessage.hidden = false;
+
+    form.classList.add("error");
+
+}
+
+function hiddenError(input) {
+
+    const errorMessage = document.querySelector(`#${input.id}-error-message`);
+
+    errorMessage.textContent = "";
+
+    errorMessage.hidden = true;
+
+    input.classList.remove("error");
+
+    form.classList.remove("error");
+
+}
